2da3efa2dfffafb4749bd692fe52197227c4cfa5
chunkychunk2 <49397604+chunkychunk2@users.noreply.github.com>
2021-12-16 09:50:20 +0300

    add 2 files (Main.java and GetLinks.java
    Main class contains main method
    GetLinks class contains parser logic

---
+++ GetLinks.java
@@ -0,0 +1,62 @@
+package main.java.com.company;
+
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class GetLinks {
+    private final ArrayList<String> lines = new ArrayList<>();
+
+    public boolean notYetVisited(String link) {
+        return !lines.contains(link);
+    }
+
+    public void  linksParser(String site, int max) throws IOException {
+        Document doc = Jsoup.connect(site).get();
+        Elements links = doc.select("a[href]");
+        for (Element link : links) {
+            String absLink = link.attr("abs:href");
+            if (absLink.contains(site) && notYetVisited(absLink)) {
+                lines.add(absLink);
+            }
+        }
+        ArrayList<String> lines2 = new ArrayList<>();
+        for (int j = 0; j < max-1; j++) {
+            for (String line : lines) {
+                try {
+                    Document doc2 = Jsoup.connect(line).get();
+                    Elements links2 = doc2.select("a[href]");
+                    for (Element link : links2) {
+                        String absLink = link.attr("abs:href");
+
+                        if (absLink.contains(site) && notYetVisited(absLink)) {
+                            lines2.add(absLink);
+                        }
+                    }
+                } catch (HttpStatusException e) {
+                    lines2.add("404 " + line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            lines.addAll(lines2);
+        }
+        Set<String> set = new HashSet<>(lines);
+        lines.clear();
+        lines.addAll(set);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("/Users/user/Desktop/DemonSeo/src/main/java/result"))){
+            for (String l:lines) writer.write(l + "\n");} catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Готово!");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,77 @@
+package main.java.com.company;
+
+import java.io.*;
+import java.util.*;
+
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Сайт");
+        String site = reader.readLine();
+        System.out.println("Уровень вложения:");
+        int max = Integer.parseInt(reader.readLine());
+        reader.close();
+        GetLinks gl = new GetLinks();
+        gl.linksParser(site, max);
+
+
+
+
+
+//        System.out.println(document);
+
+//        String title = document.title();
+//        String h1 = document.body().getElementsByTag("h1").text();
+//        String description = document.head().getElementsByTag("description").text();
+//
+//        System.out.println(title);
+//        System.out.println(h1);
+//        System.out.println(description);
+
+//        Elements h1 = document.select("h1");
+//        Elements titleElem = document.select("head > title");
+//        Elements descElem = document.select("meta");
+//        String [] meta = descElem.toString().split("\n");
+//        for (String description: meta){
+//            if (description.startsWith("<meta name=\"description\"")) System.out.println(description);
+//        }
+//        System.out.println(h1);
+//        System.out.println(titleElem);
+
+
+        //  System.out.println(descElem);
+//
+//        String HTMLSTring = "<!DOCTYPE html>"
+//                + "<html>"
+//                + "<head>"
+//                + "<title>Новый мировой стандарт освещения | ЦЕНТРСВЕТ</title>"
+//                + "</head>"
+//                + "<body>"
+//                + "<table><tr><td><h1>HelloWorld</h1></tr>"
+//                + "</table>"
+//                + "</body>"
+//                + "</html>";
+//
+//        Document newHtml = Jsoup.parse(HTMLSTring);
+//
+//        title = newHtml.title();
+//        h1 = newHtml.body().getElementsByTag("h1").text();
+//
+//        System.out.println(title);
+//        System.out.println(h1);
+//       System.out.println(description);
+
+
+    }
+
+}
